0f9c74d09fd6fba22803bc21dd4f1835ef94a7f1
Sho Hashimoto <hashimoto@shokai.org>
2011-11-20 18:02:06 +0900

    mkdir helpers

--- bootstrap.rb
+++ bootstrap.rb
@@ -6,7 +6,7 @@ require 'haml'
 require 'yaml'
 require 'json'
 require 'memcached'
-[:models, :controllers].each do |dir|
+[:helpers, :models, :controllers].each do |dir|
   Dir.glob(File.dirname(__FILE__)+"/#{dir}/*.rb").each do |rb|
     puts "loading #{rb}"
     require rb
@@ -28,8 +28,4 @@ rescue => e
   STDERR.puts 'memcached error'
   STDERR.puts e
   exit 1
-end
-
-def app_root
-  "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
 end
