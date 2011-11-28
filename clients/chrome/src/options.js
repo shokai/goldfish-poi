36773e80be500f9798ab69ce1cef9135dd9ef9dd
Sho Hashimoto <hashimoto@shokai.org>
2011-11-28 17:32:38 +0900

    set nfc_tag default value

--- clients/chrome/src/options.js
+++ clients/chrome/src/options.js
@@ -1,6 +1,6 @@
 $(
     function(){
-        $('#nfc_tag').val(localStorage.nfc_tag);
+        $('#nfc_tag').val(localStorage.nfc_tag||"a1b2ce34df");
         $('#btn_save').click(
             function(){
                 localStorage.nfc_tag = $('#nfc_tag').val();
