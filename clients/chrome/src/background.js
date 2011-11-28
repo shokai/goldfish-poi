df7921b4255ba19d93b8ee1b706aac2f3a68e16a
Sho Hashimoto <hashimoto@shokai.org>
2011-11-28 21:22:47 +0900

    check received comet data

--- clients/chrome/src/background.js
+++ clients/chrome/src/background.js
@@ -1,6 +1,12 @@
 var api_url = "http://ubif.org:8932";
 // var api_url = "http://localhost:8932"
 
+$(
+    function(){
+        comet_get();
+    }
+);
+
 chrome.self.onConnect.addListener(
     function(port, name){
         port.onMessage.addListener(
@@ -15,17 +21,21 @@ chrome.self.onConnect.addListener(
     }
 );
 
-
 var comet_get = function(){
     var url = api_url+"/"+localStorage.nfc_tag;
-    console.log('comet_get =>' + url);
+    console.log('comet_get : ' + url);
     $.ajax(
         {
             url : url,
             success : function(data){
                 console.log("comet received : "+data);
-                if(data.match(/^https?:\/\/.+/)){
-                    window.open(data);
+                if(data && data.length > 0){
+                    if(data.match(/^https?:\/\/.+/)){
+                        window.open(data);
+                    }
+                }
+                else{
+                    console.log('comet error : received data null');
                 }
             },
             error : function(e){
@@ -40,8 +50,3 @@ var comet_get = function(){
     );
 };
 
-$(
-    function(){
-        comet_get();
-    }
-);
