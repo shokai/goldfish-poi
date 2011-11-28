6e6a050a83ac5742b75dfd7a4abe36c9db12e932
Sho Hashimoto <hashimoto@shokai.org>
2011-11-29 02:07:01 +0900

    notify chrome ext nfc_tag saved

--- clients/chrome/src/options.js
+++ clients/chrome/src/options.js
@@ -1,10 +1,20 @@
+var log = function(msg){
+    console.log(msg);
+    $('#notify').html(msg).fadeIn(
+        'slow',
+        function(){
+            $(this).fadeOut(1000);
+        }
+    );
+};
+
 $(
     function(){
-        $('#nfc_tag').val(localStorage.nfc_tag||"a1b2ce34df");
+        $('#nfc_tag').val(localStorage.nfc_tag||"your-nfc-tag-id");
         $('#btn_save').click(
             function(){
                 localStorage.nfc_tag = $('#nfc_tag').val();
-                alert('saved!');
+                log('saved!');
             }
         );
     }
